8aba36fd1128d8f00bbf99d6c563c12b1db103fb
Igor Pestov <ipestov.exceedteam@gmail.com>
2020-07-29 18:46:53 +0300

    display item on page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
-import { InputBase, Container } from "@material-ui/core";
+import {
+  InputBase,
+  Container,
+  Paper,
+  Grid,
+  Typography,
+  Link,
+  GridList,
+  GridListTile,
+  ButtonBase,
+} from "@material-ui/core";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
 import APIHelper from "./APIhelper";
@@ -7,6 +17,22 @@ const useStyles = makeStyles((theme) => ({
   search: {
     root: {
       flexGrow: 1,
+    },
+    GridItem: {
+      textAlign: "center",
+    },
+    image: {
+    
+    },
+    itemImg: {
+      margin: 'auto',
+      textAlign: 'center',
+      maxWidth: '50%',
+      maxHeight: '70%',
+    },
+    ListItem: {
+      // position: "absolute"
+      // hight: "auto",
     },
     position: "relative",
     borderRadius: theme.shape.borderRadius,
@@ -49,13 +75,15 @@ const useStyles = makeStyles((theme) => ({
 }));
 const App = () => {
   const [text, setText] = useState("");
+  const [items, setItems] = useState([]);
   const classes = useStyles();
-  const send = (postData) => {
-   const newData = APIHelper.send(postData);
-   console.log(newData)
+  const send = async (postData) => {
+    const items = await APIHelper.send(postData);
+    console.log(items);
+    setItems(items);
   };
   const sendHendler = (e) => {
-    if (e.key === "Enter") { 
+    if (e.key === "Enter") {
       if (text.length > 1) {
         send(text);
         setText("");
@@ -66,13 +94,6 @@ const App = () => {
     setText(e.target.value);
   };
 
-  const Render = () => {
-    return (
-      <div>
-
-      </div>
-    )
-  }
   return (
     <Container className={classes.root}>
       <div className={classes.search}>
@@ -91,7 +112,27 @@ const App = () => {
           onKeyPress={sendHendler}
         />
       </div>
-      <Render/>
+      <Grid container item xs={12} spacing={2} direction="row">
+        {items.map((item, id) => {
+          return (
+            <Grid key={id} item xs={3} className={classes.GridItem}>
+              <Paper className={classes.ListItem}>
+                <ButtonBase href={item.url} className={classes.image} style={{width:"100%"}} >
+                  <div
+                    style={{backgroundImage: `url(${item.img})`, backgroundRepeat: 'no-repeat', backgroundSize: 'contain', backgroundPosition: 'center', width: 250,
+                    height: 300 , margin: "auto"}}
+                  >
+                  </div>
+                </ButtonBase>
+                <Typography noWrap>
+                  <Link href={item.url}>{item.title}</Link>
+                </Typography>
+                <Typography>{item.price}</Typography>
+              </Paper>
+            </Grid>
+          );
+        })}
+      </Grid>
     </Container>
   );
 };
